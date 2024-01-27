9597ab27263de8dd0aeb1bdde69ac5b848be4d8d
betthink <robetszone@gmail.com>
2024-01-27 21:21:26 +0700

    feat : menampilkan list belum bayar dari rt maupun semua

---
+++ src/api/ipl/list-belum-lunas.php
@@ -0,0 +1,49 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+// Izinkan metode HTTP tertentu
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+
+// Izinkan header kustom yang mungkin digunakan dalam permintaan Anda
+header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization");
+require "../../../config/connect.php";
+
+// 1. Ambil semua pengguna dari tabel user
+$usersQuery = "SELECT lpi.id_user, dp.nama, dp.rt 
+               FROM list_pembayar_iuran AS lpi
+               INNER JOIN data_penduduk AS dp ON lpi.id_user = dp.id_penduduk";
+$usersResult = mysqli_query($conn, $usersQuery);
+// var_dump($usersResult['rt']);die;
+$response = [];
+
+// 2. Untuk setiap pengguna
+while ($user = mysqli_fetch_assoc($usersResult)) {
+    $userId = $user['id_user'];
+    $userName = $user['nama'];
+
+    // 3. Periksa entri pembayaran untuk bulan dan tahun saat ini
+    $currentMonth = date('m');
+    $currentYear = date('Y');
+    $paymentQuery = "SELECT COUNT(*) as total FROM riwayat_pembayaran
+                     WHERE id_user = $userId
+                     AND MONTH(waktu_verifikasi) = $currentMonth
+                     AND YEAR(waktu_verifikasi) = $currentYear";
+
+    $paymentResult = mysqli_query($conn, $paymentQuery);
+    $paymentData = mysqli_fetch_assoc($paymentResult);
+    $totalPayments = $paymentData['total'];
+
+    // 4. Tandai sebagai "lunas" atau "belum lunas" dan tambahkan ke respons
+    $status = ($totalPayments > 0) ? 'lunas' : 'belum lunas';
+
+    $response[] = [
+        'id_user' => $userId,
+        'user_name' => $userName,
+        'status' => $status,
+    ];
+}
+
+// Kembalikan data dalam bentuk JSON
+echo json_encode($response);
--- src/api/ipl/riwayat-transaksi-keuangan-ipl-by-rt.php
+++ src/api/ipl/riwayat-transaksi-keuangan-ipl-by-rt.php
@@ -2,7 +2,7 @@
 require "../../../config/connect.php";
 require "../../utils/getDataJson.php";
 $rt = $_GET['rt'];
-$columns = ["metode", "verifikator", "rt", "jumlah_transaksi", "jenis_transaksi", "waktu_verifikasi"];
+$columns = ["metode", "verifikator", "rt", "jumlah_transaksi", "jenis_transaksi", "waktu_verifikasi", "nama"];
 $query = "SELECT *  FROM riwayat_pembayaran WHERE rt = '$rt'";
 // var_dump($conn);
 $getData = new GetData($conn, $columns, $query);
