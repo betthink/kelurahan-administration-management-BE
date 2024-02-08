9aa464394550ee23d30f7b8a8f4f0281c72d4162
betthink <robetszone@gmail.com>
2024-02-08 14:17:52 +0700

    feat : (IPL) - menampilkan hanya user yang valid

--- config/header.php
+++ config/header.php
@@ -1,10 +1,10 @@
 
 <?php
-$allowed_domains = array("http://localhost:3000", "https://management-administrasi-kelurahan.vercel.app", "https://appfordev.com", "http://localhost");
-$origin = $_SERVER['HTTP_ORIGIN'];
-if (in_array($origin, $allowed_domains)) {
-    header("Access-Control-Allow-Origin: " . $origin);
-}
+// $allowed_domains = array("http://localhost:3000", "https://management-administrasi-kelurahan.vercel.app", "https://appfordev.com", "http://localhost");
+// $origin = $_SERVER['HTTP_ORIGIN'];
+// // if (in_array($origin, $allowed_domains)) {
+// //     header("Access-Control-Allow-Origin: " . $origin);
+// // }
 // Izinkan metode HTTP tertentu
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization");
--- src/utils/updateData.php
+++ src/utils/updateData.php
@@ -1,5 +1,5 @@
 <?php
-require "../../config/header.php";
+require_once __DIR__ . "/../../config/header.php";
 class UpdateData
 {
 
